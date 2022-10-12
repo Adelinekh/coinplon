28d602e73171e8bf092b27bd652f589ee2e0c6e8
Adeline El Khoury <adelineinati@gmail.com>
2022-10-12 15:48:30 +0200

    temoignage

---
+++ temoignage/admin.py
@@ -0,0 +1,15 @@
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+
+from .models import Temoignage
+
+
+@modeladmin_register
+class TemoignageAdmin(ModelAdmin):
+    model = Temoignage 
+    menu_label = "Témoignage"
+    menu_icon = "placeholder"
+    menu_order = 290
+    add_to_settings_menu =  False
+    exclude_from_explorer = False
+
+#@modeladmin_register = modeladmin_registrer(TemoinageAdmin)
